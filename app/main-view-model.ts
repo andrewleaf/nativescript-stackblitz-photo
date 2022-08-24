41f88badeba466b1a1f3dfca0970bae35ae36777
andrewleaf <andrew.leaf@gmail.com>
2022-08-24 15:41:56 -0500

    4 files modified

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -1,4 +1,5 @@
 import { Observable } from '@nativescript/core';
+import { CameraOptions, requestPermissions } from '@nativescript/camera';
 
 export class HelloWorldModel extends Observable {
   private _counter: number;
@@ -6,37 +7,12 @@ export class HelloWorldModel extends Observable {
 
   constructor() {
     super();
-
-    // Initialize default values.
-    this._counter = 42;
-    this.updateMessage();
-  }
-
-  get message(): string {
-    return this._message;
-  }
-
-  set message(value: string) {
-    if (this._message !== value) {
-      this._message = value;
-      this.notifyPropertyChange('message', value);
-    }
   }
 
   onTap() {
-    this._counter--;
-    this.updateMessage();
-  }
-
-  private updateMessage() {
-    if (this._counter <= 0) {
-      this.message =
-        'Hoorraaay! You unlocked the NativeScript clicker achievement!';
-    } else {
-      this.message = `${this._counter} taps left`;
-    }
-
-    // log the message to the console
-    console.log(this.message);
+    requestPermissions().then(
+      (x) => {},
+      (e) => {}
+    );
   }
 }
